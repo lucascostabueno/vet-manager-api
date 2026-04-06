c012d3e38cc62e2210a4911cf724b5ca00ea7b6a
lucascostabueno <lucasbueno722g@gmail.com>
2026-04-06 20:54:50 -0300

    chore: initial commit

---
+++ src/main/java/br/com/lucascostabueno/vetmanager/api/VetManagerApiApplication.java
@@ -0,0 +1,13 @@
+package br.com.lucascostabueno.vetmanager.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VetManagerApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(VetManagerApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/br/com/lucascostabueno/vetmanager/api/VetManagerApiApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.lucascostabueno.vetmanager.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VetManagerApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
