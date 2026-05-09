0a7ba754d700bda9e9351fffb9acdd523cdb0af2
lucascostabueno <lucasbueno722g@gmail.com>
2026-05-09 15:27:03 -0300

    feat(config): setup OpenAPI documentation with Swagger

---
+++ src/main/java/br/com/lucascostabueno/vetmanager/api/config/openapi/OpenApiConfig.java
@@ -0,0 +1,20 @@
+package br.com.lucascostabueno.vetmanager.api.config.openapi;
+
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class OpenApiConfig {
+
+    @Bean
+    public OpenAPI customOpenAPI() {
+        return new OpenAPI()
+                .info(new Info()
+                        .title("Vet Manager API")
+                        .version("v1")
+                        .description("Veterinary Management System"));
+    }
+
+}
--- src/main/java/br/com/lucascostabueno/vetmanager/api/modules/registration/employee/infrastructure/controller/EmployeeController.java
+++ src/main/java/br/com/lucascostabueno/vetmanager/api/modules/registration/employee/infrastructure/controller/EmployeeController.java
@@ -7,6 +7,7 @@ import br.com.lucascostabueno.vetmanager.api.modules.registration.employee.appli
 import br.com.lucascostabueno.vetmanager.api.modules.registration.employee.domain.service.impl.EmployeeServiceImpl;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
+import org.springdoc.core.annotations.ParameterObject;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.http.ResponseEntity;
@@ -23,7 +24,7 @@ public class EmployeeController {
     private final EmployeeServiceImpl service;
 
     @GetMapping("/{id}")
-    public ResponseEntity<EmployeeResponse> findById(@PathVariable UUID id) {
+    public ResponseEntity<EmployeeResponse> getById(@PathVariable UUID id) {
         return ResponseEntity.ok(service.findById(id));
     }
 
@@ -43,7 +44,7 @@ public class EmployeeController {
     }
 
     @GetMapping
-    public ResponseEntity<Page<EmployeeResponse>> search(EmployeeSearchFilter filter, Pageable pageable) {
+    public ResponseEntity<Page<EmployeeResponse>> search(@ParameterObject EmployeeSearchFilter filter, @ParameterObject Pageable pageable) {
         return ResponseEntity.ok(service.search(filter, pageable));
     }
 
