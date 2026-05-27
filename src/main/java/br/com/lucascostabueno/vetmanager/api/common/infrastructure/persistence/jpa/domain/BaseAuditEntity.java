402a2cee91f3f46b674d8f65cfec4744253cd973
lucascostabueno <lucasbueno722g@gmail.com>
2026-05-26 21:58:01 -0300

    feat(config): enable JPA Auditing in the application root

---
+++ src/main/java/br/com/lucascostabueno/vetmanager/api/common/infrastructure/persistence/jpa/domain/BaseAuditEntity.java
@@ -0,0 +1,33 @@
+package br.com.lucascostabueno.vetmanager.api.common.infrastructure.persistence.jpa.domain;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.EntityListeners;
+import jakarta.persistence.MappedSuperclass;
+import org.springframework.data.annotation.CreatedBy;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedBy;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import java.time.LocalDateTime;
+
+@MappedSuperclass
+@EntityListeners(AuditingEntityListener.class)
+public abstract class BaseAuditEntity {
+
+  @CreatedDate
+  @Column(name = "created_at", nullable = false, updatable = false)
+  private LocalDateTime createdAt;
+
+  @CreatedBy
+  @Column(name = "created_by", updatable = false)
+  private String createdBy;
+
+  @LastModifiedDate
+  @Column(name = "updated_at", nullable = false)
+  private LocalDateTime updatedAt;
+
+  @LastModifiedBy
+  @Column(name = "updated_by")
+  private String updatedBy;
+}
